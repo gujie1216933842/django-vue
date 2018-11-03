35b1d369aa0afe527b6a112cfbdd73c23f17e576
gujie1216933842 <gujientsy@163.com>
2018-11-03 13:22:45 +0800

    django-vue

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -1,23 +1,19 @@
 import json
 from django.views.generic.base import View
 from goods.models import Goods
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+
 
 class GoodsListView(View):
-    def get(self,request):
+    def get(self, request):
         """
         通过django的view实现商品的列表页
         :param request:
         :return:
         """
-        json_list = []
         goods = Goods.objects.all()[:10]
-        for good in goods:
-            json_dict = {}
-            json_dict['name'] = good.name
-            json_dict['category'] = good.category.name
-            json_dict['market_price'] = good.market_price
-            json_list.append(json_dict)
+        json_data = serializers.serialize('json', goods)
+        json_data = json.loads(json_data)
 
-
-        return HttpResponse(json.dumps(json_list),content_type="application/json")
+        return JsonResponse(json_data)
