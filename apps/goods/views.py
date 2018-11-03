1f34c7c0d7a4ac9634c3e5a65b87b40e8d68046d
gujie1216933842 <gujientsy@163.com>
2018-11-03 14:48:38 +0800

    django-vue

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializer import GoodsSerializer
+
+from .models import Goods
+
+class GoodsListView(APIView):
+    def get(self,request,format=None):
+        goods = Goods.objects.all()[:10]
+        json_data = GoodsSerializer(goods,many=True)
+        return Response(json_data.data)
+
+
